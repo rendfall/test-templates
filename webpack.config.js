0c145b3606e931fea7bd48af97807a1a89a597b2
rendfall@gmail.com <rendfall@gmail.com>
2016-06-14 02:57:19 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: __dirname + '/lib/templates.js',
+
+    output: {
+        path: __dirname + '/dist/',
+        filename: 'templates.js'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'stage-0']
+                }
+            }
+        ]
+    }
+};
